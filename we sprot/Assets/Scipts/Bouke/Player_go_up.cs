6bab2684beedb3ded8eecca00c59cb790efbcc99
Bouke <BoukeWeel@outlook.com>
2020-02-04 14:47:26 +0100

    De stairs

---
+++ we sprot/Assets/Scipts/Bouke/Player_go_up.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_go_up : MonoBehaviour
+{
+
+    public Transform[] pos;
+
+    public int traptrede = 0;
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(traptrede < 10)
+            {
+                
+                MoveUP();
+                
+            }
+            
+        }
+    }
+    public void MoveUP()
+    {
+        traptrede++;
+        transform.position = pos[traptrede].position;
+
+    }
+}
