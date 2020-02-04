d561b372e6f6afef24ff75fd43bb5782526771f2
Bouke <BoukeWeel@outlook.com>
2020-02-04 12:00:57 +0100

    probuilder

---
+++ we sprot/Assets/Scipts/Start_.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_ : MonoBehaviour
+{
+    public GameObject Start;
+    public GameObject credits;
+    public GameObject exit;
+
+    public void Startknop()
+    {
+        
+    }
+    public void CreditsKnop()
+    {
+
+    }
+    public void ExitKnop()
+    {
+
+    }
+}
